2a3e0aa96409ab175e1f813a59c3ffd815538f06
Zsófia Kővágó <47915523+zsofi777@users.noreply.github.com>
2019-11-23 17:39:50 +0100

    Update AddTodo.js

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react'
 
-//need a local state for what user type in
-//& mert state, it have to be class based component
-//extend, mert örökölje a react Component functionality-t
 class AddTodo extends Component {
   state = {
     content: ''
   }
-  //(e)=event object
-  //if change=> update the content property
-  //<input>-hoz
   handleChange = (e) => {
     this.setState({
       content: e.target.value
     });
   }
   handleSubmit = (e) => {
-    //ne frissítse az oldalt
     e.preventDefault();
-    //submit=output, kiírja az új todo-t
-    //call function to add a todo
     if (this._inputElement.value !== "") {
-      //props: App.js-ből
     this.props.addTodo(this.state);
-    //submit-ra űríti state-et, 
-    //lent inputban =-vé teszi value-val, így kitörlődik, amit bevittem value={this.state.content} 
     this.setState({
       content: ''
     })
@@ -32,8 +20,6 @@ class AddTodo extends Component {
 }
   render() {
     return (
-      //formban benne van a submit event
-      //change event when user type in input field (=when text inside change)
       <div>
         <form onSubmit={this.handleSubmit}>
           <label>Add a new todo:</label>
